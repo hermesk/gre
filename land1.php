6f90b00c78d36d368b2037c4d098fa5392617bc2
hermesk <jimikinyua@gmail.com>
2018-03-07 16:32:57 +0300

    "multiselect drop down "

--- land1.php
+++ land1.php
@@ -1,168 +1,50 @@
- <?php
+  <?php
   require 'check.php';
   require 'payment_details.php';
   require 'property_details.php';
   require 'functions.php';
-  require 'header.php';
-
-//select plotnos
-$location=$_POST['location']; 
-$size=$_POST['lsize']; 
-//$location = "MAVOLONI";
-
-function FORTY_BY_EIGHTY(){
-require  'connect.php';
- global $location;
- $sql ="SELECT FORTY_BY_EIGHTY FROM  $location WHERE FORTY_BY_EIGHTY  IS NOT NULL";
- $stmt = sqlsrv_query( $conn, $sql); 
-
- if( $stmt === false )  
-{  
-     echo "Error in executing statement.\n";  
-     die( print_r( sqlsrv_errors(), true));  
-} 
-
-else {
-
-    while($row = sqlsrv_fetch_array($stmt)){
-   
-      $ptn = '<option value = ' . $row['FORTY_BY_EIGHTY']. '>' . $row['FORTY_BY_EIGHTY'] . ' </option>';
-      echo $ptn;  
-       
-    }
-  } 
-}
-
-function EIGHTH(){
-require  'connect.php';
- global $location;
- $sql ="SELECT EIGHTH FROM  $location WHERE EIGHTH  IS NOT NULL";
- $stmt = sqlsrv_query( $conn, $sql); 
-
- if( $stmt === false )  
-{  
-     echo "Error in executing statement.\n";  
-     die( print_r( sqlsrv_errors(), true));  
-} 
-
-else {
-
-    while($row = sqlsrv_fetch_array($stmt)){
-   
-      $ptn = '<option value = ' . $row['EIGHTH']. '>' . $row['EIGHTH'] . ' </option>';
-      echo $ptn;  
-       
-    }
-  } 
-}
- function QUARTER(){
-require  'connect.php';
- global $location;
- $sql ="SELECT QUARTER FROM  $location WHERE QUARTER  IS NOT NULL";
- $stmt = sqlsrv_query( $conn, $sql); 
-
- if( $stmt === false )  
-{  
-     echo "Error in executing statement.\n";  
-     die( print_r( sqlsrv_errors(), true));  
-} 
-
-else {
-
-    while($row = sqlsrv_fetch_array($stmt)){
-   
-      $ptn = '<option value = ' . $row['QUARTER']. '>' . $row['QUARTER'] . ' </option>';
-      echo $ptn;  
-       
-    }
-  } 
-}
- function HALF(){
-require  'connect.php';
- global $location;
- $sql ="SELECT HALF FROM  $location WHERE HALF  IS NOT NULL";
- $stmt = sqlsrv_query( $conn, $sql); 
-
- if( $stmt === false )  
-{  
-     echo "Error in executing statement.\n";  
-     die( print_r( sqlsrv_errors(), true));  
-} 
-
-else {
-
-    while($row = sqlsrv_fetch_array($stmt)){
-   
-      $ptn = '<option value = ' . $row['HALF']. '>' . $row['HALF'] . ' </option>';
-      echo $ptn;  
-       
-    }
-  } 
-}
-
-function ACRE(){
-require  'connect.php';
- global $location;
- $sql ="SELECT ONE_ACRE FROM  $location WHERE ONE_ACRE  IS NOT NULL";
- $stmt = sqlsrv_query( $conn, $sql); 
-
- if( $stmt === false )  
-{  
-     echo "Error in executing statement.\n";  
-     die( print_r( sqlsrv_errors(), true));  
-} 
-
-else {
-
-    while($row = sqlsrv_fetch_array($stmt)){
-   
-      $ptn = '<option value = ' . $row['ONE_ACRE']. '>' . $row['ONE_ACRE'] . ' </option>';
-      echo $ptn;  
-       
-    }
-  } 
-}
+   require 'header.php';
+  $name= "";
 ?>
 <!DOCTYPE html>
 <html>
-
 <head>
-      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-      <link rel="stylesheet" type="text/css" href="css/bootstrap-multiselect.css">  
-       <link rel="stylesheet" type="text/css" href="css/gak.css"> 
-        <script src="js/jquery.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js//bootstrap-multiselect.js"></script>
-        
-        
-
-
+  <title>GRE Land Payment Section</title>
+    <link rel="stylesheet" type="text/css" href="css/gak.css">   
+   <script src="js/jquery.js"></script>  
+     
 </head>
+
 <body>
 <?php echo '<div align="center" style="color:#800000" >GAKUYO LAND PAYMENTS</div>'; ?>
 <div class="land">
  <fieldset><legend ><label id="home"><a href="home.php" style="text-decoration:none;"><b>HOME</b></a></label></legend> 
-   <form method="post" action="land_rct.php">
-   <table  >
+ <table border="0">
+  <form method="post" action="land_rct.php">
+    <table  >
     <tr>
      <td><b>Location</b></td>
-     <td><input id="location"  name="location"  value="<?php echo $location; ?>" readonly="readonly" /><br /></td>
-    
+    <td> <select  id="location" name="location" style="width: 180px"><option>Select location</option>
+    <?php location(); ?> </select></td>
 
-     <td><b>Size</b></td>
- 
-      <td><input id="lsize"  name="lsize"  value="<?php echo $size; ?>" readonly="readonly" /><br /></td>
-     
-   
+     <td><b>Size</b></td><td>
+     <select name="lsize" id="lsize" style="width: 180px" ><option>Select Size</option>
+    <?php
+      landsize();
+    ?>
+    </select></td>
+  
+   </tr>
+   <tr>
      <td><b>ID/PP</b></td>
-    <td><input type="text" placeholder="Search by ID/plotno" name="scid" id="scid" style="width: 150px" onclick="plotno()" 
+    <td><input type="text" placeholder="Search by ID/plotno" name="scid" id="scid" onclick="plotno()" 
      onkeyup="this.value=this.value.replace(/[^a-zA-Z\s\d]/g,'');"></td>
     <td><button type="button" id="sbtn" value="Search" name="scid" formnovalidate onclick="searchclient()">Search</button></td>
- </tr>
-     <tr>
-          <td><b>NAME</b></td>
+
+
      <td colspan="3" ><input type="text" placeholder="Client name" style="width:95%;" name="cname" id="cname"  readonly="readonly"></td>
-   
+    </tr>
+     <tr>
   
   </tr>
     <tr>
@@ -172,31 +54,9 @@ else {
      <td><b>Cost</b></td>
     <td><input type="text"  name="cost" id="cost"  placeholder="Plot Cost" required ></td>
      <td><b>Plot No.</b></td>
-      <td> 
-        <select id="option-droup-demo" multiple="multiple" id="plotno">
-          <optgroup label="40 BY 80" >
-                 <?php FORTY_BY_EIGHTY(); ?>   
-               
-        </optgroup>
-        <optgroup label="1/8 Acre" id="plot">
-                 <?php EIGHTH(); ?>   
-               
-        </optgroup>
-        <optgroup label="1/4 Acre">
-      
-            <?php QUARTER(); ?>
-           
-        </optgroup>
-        <optgroup label="1/2 Acre">
-           <?php HALF(); ?>
-       
-        </optgroup>    
-        <optgroup label="1 Acre" id="acre">
-            <?php ACRE(); ?>
-       
-        </optgroup>      
-    </select>
-  </td></tr>
+      <td> <select name="plotn" id="plotn" style="width: 180px">
+       <option>Select plot no</option>
+     </select></td></tr>
      <tr>
          <td><b>Pay Type</b></td><td>
          <select name="ptype"  style="width: 180px" id="ptype"><option>Select Payment type</option>
@@ -226,7 +86,6 @@ else {
     <tr>
      <td><b>Narration</b></td>
    <td colspan="4">  <input type="text" style="width:95%" placeholder="Narration" name="nrt" id="nrt" onclick="refn()" onkeyup="this.value=this.value.replace(/[^a-zA-Z\s\d\/]/g,'');" required></td>
-     
       <td><input type="hidden" name="rctno" value="autoref()" id="rctno"></td>
       <td><input type="hidden" id="pdate" name="pdate" value="<?php echo date('Y-m-d'); ?>" /></td>
       
@@ -239,18 +98,10 @@ else {
       <td><input type="reset" id="reset"></td>
      </tr>
 </table>
- </form>
- </fieldset> 
+       <!-- search client--> 
+      <script type="text/javascript">
 
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('#option-droup-demo').multiselect({
-            enableClickableOptGroups: true,
-            enableCollapsibleOptGroups: true
-
-        });
-        });
-
+       
             var fnf = document.getElementById("amnt");
             fnf.addEventListener('keyup', function(evt){
             var n = parseInt(this.value.replace(/\D/g,''),10);
@@ -315,17 +166,19 @@ else {
 }
  
 
-      function plotno(){
+    function plotno(){
       
   
       var loc = $("#location").val();
       var s = "Select location";
       var size = $('#lsize').val();
       var sz = "Select Size";
+
       if (loc===s) {
          alert("Select location")
       }
       else if (size===sz) {
+
         alert("Select plot Size");
         event.preventDefault();
       }
@@ -335,6 +188,7 @@ else {
         function(data)
          {
           $('#plotn').html("");
+
          if (!$.trim(data)) {
             var n="Nil ";
          $('#plotn').append($('<option></option>').val(n).text(n));
@@ -348,12 +202,12 @@ else {
            
            $('#plotn').append($('<option></option>').val(a).text(a));
           $('#plotn').append(data);
-          $('#plot').append(data);
             
             
           }
          
          }
+
         );
     }
     }
@@ -554,7 +408,7 @@ else {
           
          }
          else{
-            window.location.href = "location.php";
+            window.location.href = "land.php";
             alert(" Transaction Error.Data not saved");
             alert(data);
          }
@@ -569,12 +423,16 @@ else {
          
 
   });
+ 
 
     </script>
+     
+  </form>
   
+ </table>
+ </fieldset>     
+
 </div>
-
+ 
 </body>
-
-
 </html>
